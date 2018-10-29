8fcbec22328b885c3146bdc238d952db4f6f8d1a
Manjula Jayawardana <manjulajayawardana@gmail.com>
2018-10-29 15:48:52 +0530

    add semaphore example

---
+++ basic/src/main/java/com/manjula/threadbasic/semaphores/SemaphoreApp.java
@@ -0,0 +1,42 @@
+package com.manjula.threadbasic.semaphores;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.stream.IntStream;
+
+public class SemaphoreApp {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		
+		IntStream.range(0, 12).forEach(i -> executorService.execute(() -> Downloader.INSTANCE.downloadData()));
+	}
+
+}
+
+enum Downloader {
+	INSTANCE;
+	
+	private Semaphore semaphore = new Semaphore(3, true);
+	
+	public void downloadData() {
+		try {
+			semaphore.acquire();
+			download();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaphore.release();
+		}
+	}
+
+	private void download() {
+		System.out.println("Downloading data from the web...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
